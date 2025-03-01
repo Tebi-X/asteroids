dc3fb685f53c31c6b45a5d522461d2c7ae9e4580
tebix <esteban.torres.perez@outlook.com>
2025-03-01 15:01:26 -0800

    player can move and frame limits has been set

--- player.py
+++ player.py
@@ -24,8 +24,27 @@ class Player(CircleShape):
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
 
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        new_position = self.position + forward * PLAYER_SPEED * dt
+
+        if self.in_frame_limits(new_position):
+            self.position = new_position
+
+    def in_frame_limits(self, move_position):
+        if move_position[0] > 0 and move_position[0] < SCREEN_WIDTH and move_position[1] > 0 and move_position[1] < SCREEN_HEIGHT:
+            return True
+        return False 
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        print(self.position, self.rotation)
+
+        if keys[pygame.K_s]:
+            self.move(dt * -1)
+
+        if keys[pygame.K_w]:
+            self.move(dt)
 
         if keys[pygame.K_a]:
             self.rotate(dt * -1)
