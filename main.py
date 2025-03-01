a992e71f7adb625abf2b75dc04622a2c50f36984
tebix <esteban.torres.perez@outlook.com>
2025-02-28 22:21:56 -0800

    draw player sprite

--- main.py
+++ main.py
@@ -2,9 +2,14 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+clock = pygame.time.Clock()
+dt = 0
 
 #additional files
 from constants import *
+
+#import player module
+from player import *
 
 def main():
     pygame.init()
@@ -15,13 +20,24 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    #Start player object at the middle of the screen
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    player = Player(x, y)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
 
         screen.fill((0, 0, 0))
+
+        player.draw(screen)
+
         pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
